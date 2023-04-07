3604edf3905d9958adb754f184a58b91540b7ad5
Nicolas Oriti Tizio <oritinicolas@gmail.com>
2023-04-07 20:30:06 -0300

    feat: desafio-02 fs

--- ProductManager.js
+++ ProductManager.js
@@ -1,15 +1,23 @@
+const { promises: fs } = require('fs');
+
 class ProductManager {
-  constructor() {
-    this.products = [];
+  constructor(path) {
+    this.path = path;
   }
 
-  getProducts() {
-    return this.products;
-  }
-
-  getProductById(id) {
+  async getProducts() {
     try {
-      let product = this.products.find((prod) => prod.id === id);
+      const products = await fs.readFile(this.path, 'utf-8');
+      return JSON.parse(products);
+    } catch (error) {
+      return [];
+    }
+  }
+
+  async getProductById(id) {
+    try {
+      const products = await this.getProducts();
+      let product = products.find((prod) => prod.id === id);
       if (!product) {
         throw new Error('Not found');
       }
@@ -19,9 +27,10 @@ class ProductManager {
     }
   }
 
-  addProduct(title, description, price, thumbnail, code, stock) {
+  async addProduct({ title, description, price, thumbnail, code, stock }) {
     try {
-      this.products.forEach((prod) => {
+      const products = await this.getProducts();
+      products.forEach((prod) => {
         if (prod.code === code) {
           throw new Error('El codigo ya existe');
         }
@@ -36,44 +45,81 @@ class ProductManager {
         stock,
       };
 
-      if (this.products.length === 0) {
-        product.id = 0;
-      } else {
-        product.id = this.products[this.products.length - 1].id + 1;
+      product.id = 0;
+      if (products.length > 0) {
+        product.id = products[products.length - 1].id + 1;
       }
 
-      this.products.push(product);
+      products.push(product);
+      await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async updateProduct(id, updatedFields) {
+    try {
+      const products = await this.getProducts();
+      const index = products.findIndex((prod) => prod.id === id);
+      if (index === -1) {
+        throw new Error('Not found');
+      }
+
+      const productToUpdate = { ...products[index], ...updatedFields };
+
+      products[index] = productToUpdate;
+
+      await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+
+      return productToUpdate;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async deleteProduct(id) {
+    try {
+      const products = await this.getProducts();
+      const index = products.findIndex((prod) => prod.id === id);
+      if (index === -1) {
+        throw new Error('Not found');
+      }
+
+      // Elimina el producto de la lista
+      products.splice(index, 1);
+
+      // Escribe la lista actualizada en el archivo
+      await fs.writeFile(this.path, JSON.stringify(products, null, 2));
     } catch (error) {
       console.error(error);
     }
   }
 }
 
-const productManager1 = new ProductManager();
-// console.log(productManager1.getProducts());
-productManager1.addProduct(
-  'producto prueba',
-  'Este es un producto prueba',
-  200,
-  'https://hola.png',
-  'abc123',
-  25
-);
-// productManager1.addProduct(
-//   'producto prueba',
-//   'Este es un producto prueba',
-//   200,
-//   'https://hola.png',
-//   'abc123',
-//   25
-// );
-productManager1.addProduct(
-  'producto prueba 2',
-  'Este es un producto prueba 2',
-  400,
-  'https://hola2.png',
-  'abc125',
-  25
-);
-console.log(productManager1.getProducts());
-console.log(productManager1.getProductById(3));
+const test = async () => {
+  const productManager1 = new ProductManager('./productos.json');
+  console.log('Deberia devolver un arreglo vacio:', await productManager1.getProducts());
+  await productManager1.addProduct({
+    title: 'producto prueba',
+    description: 'Este es un producto prueba',
+    price: 200,
+    thumbnail: 'Sin imagen',
+    code: 'abc123',
+    stock: 25,
+  });
+  console.log('Ahora con un producto', await productManager1.getProducts());
+  console.log(await productManager1.getProductById(3));
+  console.log(
+    'Este deberia devolver el producto con id 0: ',
+    await productManager1.getProductById(0)
+  );
+  await productManager1.updateProduct(0, { title: 'Nuevo titulo de prueba' });
+  console.log(
+    'Este deberia devolver el producto con id 0 y el titulo actualizado: ',
+    await productManager1.getProductById(0)
+  );
+  await productManager1.deleteProduct(0);
+  console.log('Este deberia devolver un arreglo vacio:', await productManager1.getProducts());
+};
+
+test();
